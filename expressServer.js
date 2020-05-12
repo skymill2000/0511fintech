6ee9561daa039374a775ef3ae08a9e44772887c0
skymill2000 <skymill2000@gmail.com>
2020-05-12 16:20:59 +0900

    ejs

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
  
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
 app.get('/', function (req, res) {
     res.send('Hello Express')
 })
