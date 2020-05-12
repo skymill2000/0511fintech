bf8444b025627ec96406e8a16f689769edda5ffa
skymill2000 <skymill2000@gmail.com>
2020-05-12 10:28:07 +0900

    init commit

---
+++ example01.js
@@ -0,0 +1,16 @@
+var length = 16; // Number
+var lastName = "Johnson"; // String
+var obj = { firstName: "John", lastName: "Doe" }; // Object
+
+console.log(obj)
+
+var x = 16 + "Volvo";
+console.log(x);
+var y = "16" + "Volvo";
+console.log(y);
+var z = 16 + "Volvo";
+console.log(z);
+var a = "Volvo" + 16;
+console.log(a);
+var b = 16 + 4 + "Volvo";
+console.log(b);
