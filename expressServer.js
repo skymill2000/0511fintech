e2917ff9472c5ce091187ea4ade827315637e18b
skymill2000 <skymill2000@gmail.com>
2020-05-12 15:23:34 +0900

    express response

--- expressServer.js
+++ expressServer.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello Express')
+    res.send('Hello Express')
 })
- 
+
+app.get('/test', function(req, res){
+    res.send('Test')
+})
+
 app.listen(3000)
