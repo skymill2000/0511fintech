75d12f7e0fbc9881aefafb84e59c624ca4cfaa6c
skymill2000 <skymill2000@gmail.com>
2020-05-12 16:01:51 +0900

    dbconn

--- mysqlExample.js
+++ mysqlExample.js
@@ -3,12 +3,12 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '1q2w3e4r',
-  database : 'my_db'
+  database : 'fintech'
 });
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+connection.query('SELECT 1 + 4 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
