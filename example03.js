593df270a1265555437b2254691c052cf573ea3c
skymill2000 <skymill2000@gmail.com>
2020-05-12 10:51:45 +0900

    example 03 added

---
+++ example03.js
@@ -0,0 +1,22 @@
+function multi(p1, p2) {
+	return p1 * p2; // p1, p2 곱연산의 결과를 반환한다.
+}
+
+function plus(p1, p2) {
+	return p1 + p2; // p1, p2 곱연산의 결과를 반환한다.
+}
+
+function minus(p1, p2) {
+	return p1 - p2; // p1, p2 곱연산의 결과를 반환한다.
+}
+
+function div(p1, p2) {
+	return p1 / p2; // p1, p2 곱연산의 결과를 반환한다.
+}
+
+
+//#work1 multi, div, plus, minus 4 function add
+console.log(multi(2, 3));
+console.log(plus(2, 3));
+console.log(div(2, 3));
+console.log(minus(2, 3));
