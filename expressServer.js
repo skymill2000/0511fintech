4efc2acef4f65e3f4800c53b3ecf5e4168cdda71
skymill2000 <skymill2000@gmail.com>
2020-05-12 16:37:14 +0900

    html / css / js

--- expressServer.js
+++ expressServer.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
- 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const path = require('path')
 
+app.set('views', path.join(__dirname, 'views')); // ejs file location
+app.set('view engine', 'ejs'); //select view templet engine
 
 app.get('/', function (req, res) {
-    res.send('Hello Express')
+    var title = "javascript"
+    res.send('<html><h1>'+title+'</h1><h2>contents</h2></html>')
 })
+
+app.get('/ejs', function(req, res){
+    res.render('test')
+})
+
 
 app.get('/test', function(req, res){
     res.send('Test')
