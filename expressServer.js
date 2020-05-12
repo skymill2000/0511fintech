b21cb69cd8f52d9cf02c7fafddc3b2859dda3385
skymill2000 <skymill2000@gmail.com>
2020-05-12 16:57:01 +0900

    design file added

--- expressServer.js
+++ expressServer.js
@@ -5,6 +5,9 @@ const path = require('path')
 app.set('views', path.join(__dirname, 'views')); // ejs file location
 app.set('view engine', 'ejs'); //select view templet engine
 
+app.use(express.static(path.join(__dirname, 'public')));//to use static asset
+
+
 app.get('/', function (req, res) {
     var title = "javascript"
     res.send('<html><h1>'+title+'</h1><h2>contents</h2></html>')
@@ -14,9 +17,12 @@ app.get('/ejs', function(req, res){
     res.render('test')
 })
 
-
 app.get('/test', function(req, res){
     res.send('Test')
+})
+
+app.get('/design', function(req, res){
+    res.render('designTest');
 })
 
 app.listen(3000)
