76d264bd9859c5efdd5283e32187f747a245b6dd
gwanwoo-you <skymill2000@gmail.com>
2020-05-15 14:22:11 +0900

    qr코드 리더기 라이브러리 설치

--- expressServer.js
+++ expressServer.js
@@ -83,6 +83,10 @@ app.get('/qrcode', function(req, res){
     res.render('qrcode');
 })
 
+app.get('/qr', function (req, res) {
+    res.render('qrReader')
+})
+
 
 
 app.get('/authResult', function(req, res){
