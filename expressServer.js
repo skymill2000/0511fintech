f41ba21a14306af2fa675ba08e9beed9c9b4b924
gwanwoo-you <skymill2000@gmail.com>
2020-05-15 14:12:14 +0900

    qr코드 생성 완료

--- expressServer.js
+++ expressServer.js
@@ -79,6 +79,11 @@ app.get('/balance', function(req, res){
     res.render('balance');
 })
 
+app.get('/qrcode', function(req, res){
+    res.render('qrcode');
+})
+
+
 
 app.get('/authResult', function(req, res){
     var authCode = req.query.code
